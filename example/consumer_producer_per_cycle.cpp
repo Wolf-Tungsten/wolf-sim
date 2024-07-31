94f8602bca55b5fd703bd25d142f64753c27a53c
wolf-tungsten <ncclcommfailed@qq.com>
2024-07-31 13:38:27 +0800

    add consumer_producer_per_cycle.cpp

--- example/consumer_producer_per_cycle.cpp
+++ example/consumer_producer_per_cycle.cpp
@@ -13,28 +13,24 @@ class Producer : public wolf_sim::Module {
 
  private:
   /* 根据需要定义的状态 */
-  int payload = 0;
+  int payload;
   /* 描述模块行为的 fire 函数 */
   void fire() {
     if (whatTime() == 0) {
-      payloadOPort << payload;
+      payload = 0;
     } else {
-      if (busyIPort.valid()) {
-        // t-1 时刻 consumer 处于 busy 状态，所以 t 时刻 Producer 还是发送 payload
-        payloadOPort << payload;
-      } else {
+      if (!busyIPort.valid()) {
         // t-1 时刻 consumer 出于 idle 状态，payload 发送成功
         std::cout << "Producer sent " << payload << " at " << whatTime() - 1
                   << std::endl;
         // t 时刻发送下一个 payload
-        ++payload;
-        if(payload < TOTAL_PAYLOAD){
-          payloadOPort << payload;
-        } else {
+        payload++;
+        if(payload >= TOTAL_PAYLOAD){
           terminateModuleSimulation();
         }
       }
     }
+    payloadOPort << payload;
     planWakeUp(1);
   };
 };
@@ -60,11 +56,7 @@ class Consumer : public wolf_sim::Module {
           terminateSimulation();
         }
         busy = false;
-      } else {
-        // 模拟处理中
-        busyOPort << true;
-        planWakeUp(1);
-      }
+      } 
     } else {
       if (payloadIPort >> pendingPayload) {
         if (PROCESS_DELAY == 0) {
@@ -79,11 +71,11 @@ class Consumer : public wolf_sim::Module {
           // 模拟处理中
           busy = true;
           doneTime = whatTime() + PROCESS_DELAY;
-          busyOPort << true;
-          planWakeUp(1);
         }
       }
     }
+    busyOPort << busy;
+    planWakeUp(1);
   }
 };
 
